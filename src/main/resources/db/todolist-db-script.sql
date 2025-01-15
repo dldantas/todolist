dc78e732f5ea7f3ae427ee271adba8a218492420
Daniel <dldantas.apps@gmail.com>
2025-01-15 17:35:59 -0300

    update db script

--- src/main/resources/db/todolist-db-script.sql
+++ src/main/resources/db/todolist-db-script.sql
@@ -9,53 +9,62 @@ USE TodoListDB;
 GO
 
 -- Create Users table
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[User]') AND type in (N'U'))
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[user]') AND type in (N'U'))
 BEGIN
-    CREATE TABLE Users (
-        UserId INT IDENTITY(1,1) PRIMARY KEY,
-        Username NVARCHAR(50) NOT NULL UNIQUE,
-        Email NVARCHAR(100) NOT NULL UNIQUE,
-        Password NVARCHAR(100) NOT NULL,
-        UpdatedDate DATETIME DEFAULT GETDATE()
-    );
+	CREATE TABLE TodoListDB.dbo.todo_user (
+		user_id bigint IDENTITY(1,1) NOT NULL,
+		username varchar(50) NOT NULL,
+		email varchar(100) NOT NULL,
+		password varchar(100) NOT NULL,
+		updated_date datetime2(6) NOT NULL,
+
+		CONSTRAINT PK__todo_use__B9BE370F116677F8 PRIMARY KEY (user_id),
+		CONSTRAINT UKa87ptdjn6xs9wxsofa7f2ui4b UNIQUE (email),
+		CONSTRAINT UKhlxrl33a7px5wmvgm5o1abx3r UNIQUE (username)
+	);
 END
 GO
 
 -- Create TodoLists table
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TodoList]') AND type in (N'U'))
 BEGIN
-    CREATE TABLE TodoLists (
-        ListId INT IDENTITY(1,1) PRIMARY KEY,
-        UserId INT NOT NULL,
-        Title NVARCHAR(100) NOT NULL,
-        Description NVARCHAR(500),
-        UpdatedDate DATETIME DEFAULT GETDATE(),
-        FOREIGN KEY (UserId) REFERENCES Users(UserId)
-    );
+	CREATE TABLE TodoListDB.dbo.todo_list (
+		list_id bigint IDENTITY(1,1) NOT NULL,
+		description varchar(500)  NULL,
+		title varchar(100) NOT NULL,
+		updated_date datetime2(6) NOT NULL,
+		user_id bigint NOT NULL,
+		CONSTRAINT PK__todo_lis__7B9EF135E0656893 PRIMARY KEY (list_id),
+		CONSTRAINT FKg8dil33hvmx3oiep445vejvab FOREIGN KEY (user_id) REFERENCES TodoListDB.dbo.todo_user(user_id)
+	);
 END
 GO
 
 -- Create TodoItems table
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TodoItem]') AND type in (N'U'))
 BEGIN
-    CREATE TABLE TodoItems (
-        ItemId INT IDENTITY(1,1) PRIMARY KEY,
-        ListId INT NOT NULL,
-        Title NVARCHAR(100) NOT NULL,
-        Description NVARCHAR(500),
-        DueDate DATETIME,
-        Priority INT DEFAULT 0,
-        IsCompleted BIT DEFAULT 0,
-        UpdatedDate DATETIME DEFAULT GETDATE(),
-        FOREIGN KEY (ListId) REFERENCES TodoLists(ListId)
-    );
+	CREATE TABLE TodoListDB.dbo.todo_item (
+		item_id bigint IDENTITY(1,1) NOT NULL,
+		description varchar(500)  NULL,
+		due_date datetime2(6) NULL,
+		is_completed bit NOT NULL,
+		priority int NULL,
+		title varchar(100) NOT NULL,
+		updated_date datetime2(6) NOT NULL,
+		list_id bigint NOT NULL,
+		CONSTRAINT PK__todo_ite__52020FDD346724D9 PRIMARY KEY (item_id),
+		CONSTRAINT FK8ugl4ng70y2cbc3926pu0h9hw FOREIGN KEY (list_id) REFERENCES TodoListDB.dbo.todo_list(list_id)
+	);
+	 CREATE NONCLUSTERED INDEX ix_todo_item_list_id ON dbo.todo_item (  list_id ASC  )  
+		 WITH (  PAD_INDEX = OFF ,FILLFACTOR = 100  ,SORT_IN_TEMPDB = OFF , IGNORE_DUP_KEY = OFF , STATISTICS_NORECOMPUTE = OFF , ONLINE = OFF , ALLOW_ROW_LOCKS = ON , ALLOW_PAGE_LOCKS = ON  )
+		 ON [PRIMARY ] ;
 END
 GO
 
 
 -- Create index for faster queries
-IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'IX_TodoItems_ListId' AND object_id = OBJECT_ID('TodoItems'))
+IF NOT EXISTS (SELECT * FROM sys.indexes WHERE name = 'ix_todo_item_list_id' AND object_id = OBJECT_ID('todo_item'))
 BEGIN
-    CREATE INDEX IX_TodoItems_ListId ON TodoItems(ListId);
+    CREATE INDEX ix_todo_item_list_id ON todo_item(list_id);
 END
 GO
